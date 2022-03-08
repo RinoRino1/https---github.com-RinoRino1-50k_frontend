f3c961ebcdb0c38d237d3c8717a0d3d434c58fe8
RinoRino1 <ahlidinovna@gmail.com>
2022-03-08 21:48:21 +0600

    homework28

--- lesson28 Mamatova Meerim Frontend-6/script/add_to_cart.js
+++ lesson28 Mamatova Meerim Frontend-6/script/add_to_cart.js
@@ -1,3 +1,9 @@
 
 let ttl = document.getElementsByClassName("ttl");
 console.log(ttl[1].innerText);
+
+let price = document.getElementsByClassName("price");
+console.log(price[0].innerText);
+
+let img = document.getElementsByClassName("img");
+console.log(img[0].innerText);
